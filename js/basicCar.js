c58baf7ceb995127ec572778345532563f0bfd04
Anfifo <afonseca-1995@hotmail.com>
2017-09-28 16:00:34 +0100

    OOP basicCar modifications

--- js/basicCar.js
+++ js/basicCar.js
@@ -1,3 +1,54 @@
+/**
+ * Class basicCar
+ * vector position for car (keep in mind car's center point is inside the car):
+ * @param x
+ * @param y
+ * @param z
+ */
+
+function showCar(x, y, z) {
+    var car = new BasicCar(x,y,z);
+    car.scale.set(10,10,10);
+    scene.add( car );
+}
+
+function createBasicCar(x,y,z){
+    return new BasicCar(x,y,z);
+}
+
+function BasicCar(x,y,z){
+
+    'use strict';
+    this.car = new THREE.Object3D();
+
+    var carLength = 13;
+    var carBlockHeight = 1;
+
+    var carWidth = 5;
+    var roofLength = 3;
+    var roofHeight = carBlockHeight + roofLength/2;
+
+    var windowPosition = roofLength/2 + carBlockHeight;
+    var wheelPosition = roofLength/2 + carBlockHeight;
+    var wheelWidth = carBlockHeight/3;
+
+    var wheelRadius = (carBlockHeight*2)/3;
+    var car = this.car;
+
+    material = new THREE.MeshBasicMaterial({ color: 0xff1a8c, wireframe: false });
+
+    addLowerBody(car, x, y, z, carLength, carBlockHeight, carWidth);
+
+    addCarRoof(car, x, y, z, carBlockHeight, carWidth, roofLength, roofHeight, windowPosition);
+
+    addCarWheels(car, x, y, z, carBlockHeight, wheelPosition, wheelRadius, wheelWidth, carWidth);
+
+    return car;
+}
+
+BasicCar.protype = Object.create(THREE.Object3D());
+
+
 function createTriangularPrism(side, length){
 
     var triangle = new THREE.Shape();
@@ -19,58 +70,30 @@ function createTriangularPrism(side, length){
 }
 
 
-function createBasicCar(x,y,z){
-
-    'use strict';
-    var carLength = 13;
-    var carBlockHeight = 1;
-    var carWidth = 5;
-
-    var roofLength = 3;
-    var roofHeight = carBlockHeight + roofLength/2;
-    var windowPosition = roofLength/2 + carBlockHeight;
-
-    var wheelPosition = roofLength/2 + carBlockHeight;
-    var wheelWidth = carBlockHeight/3;
-    var wheelRadius = (carBlockHeight*2)/3;
-
-    var car = new THREE.Object3D();
-    material = new THREE.MeshBasicMaterial({ color: 0xff1a8c, wireframe: false });
-
-    addLowerBody(car, x, y, z, carLength, carBlockHeight, carWidth);
-
-    addCarRoof(car, x, y, z, carBlockHeight, carWidth, roofLength, roofHeight, windowPosition);
-
-    addCarWheels(car, x, y, z, carBlockHeight, wheelPosition, wheelRadius, wheelWidth, carWidth);
-
-    return car;
-}
-
-
 function addLowerBody(obj, x, y, z, carLength, carBlockHeight, carWidth){
 
     //lower bottom
-    geometry = new THREE.CubeGeometry(carLength, carBlockHeight, carWidth);
-    var bottom = new THREE.Mesh(geometry, material);
+    var carGeometry = new THREE.CubeGeometry(carLength, carBlockHeight, carWidth);
+    var bottom = new THREE.Mesh(carGeometry, material);
     bottom.position.set(x,y,z);
     obj.add(bottom);
 
 
     //lower top
-    geometry = new THREE.CubeGeometry(carLength - 2*carBlockHeight, carBlockHeight, carWidth);
-    var top = new THREE.Mesh(geometry, material);
+    carGeometry = new THREE.CubeGeometry(carLength - 2*carBlockHeight, carBlockHeight, carWidth);
+    var top = new THREE.Mesh(carGeometry, material);
     top. position.set(x, y+carBlockHeight, z);
     obj.add(top);
 
     //top prism ending positive
-    geometry = createTriangularPrism(carBlockHeight, carWidth);
-    var carFront = new THREE.Mesh(geometry, material);
+    carGeometry = createTriangularPrism(carBlockHeight, carWidth);
+    var carFront = new THREE.Mesh(carGeometry, material);
     carFront.position.set(x + ((carLength - carBlockHeight)/2), y + carBlockHeight , z);
     obj.add(carFront);
 
     //top prism ending negative
-    geometry = createTriangularPrism(carBlockHeight,carWidth);
-    var carBack = new THREE.Mesh(geometry, material);
+    carGeometry = createTriangularPrism(carBlockHeight,carWidth);
+    var carBack = new THREE.Mesh(carGeometry, material);
     carBack.rotateY(Math.PI);
     carBack.position.set( x - ((carLength - carBlockHeight)/2) , y + carBlockHeight , z);
     obj.add(carBack);
@@ -82,8 +105,8 @@ function addLowerBody(obj, x, y, z, carLength, carBlockHeight, carWidth){
 function addCarRoof(obj, x, y, z, carBlockHeight, carWidth, roofLength, roofHeight, windowPosition){
 
     //roof
-    geometry = new THREE.CubeGeometry( roofLength , 2*carBlockHeight, carWidth);
-    var roof = new THREE.Mesh(geometry, material);
+    var carGeometry = new THREE.CubeGeometry( roofLength , 2*carBlockHeight, carWidth);
+    var roof = new THREE.Mesh(carGeometry, material);
     roof.position.set(x, y + roofHeight, z);
     obj.add(roof);
 
@@ -91,38 +114,39 @@ function addCarRoof(obj, x, y, z, carBlockHeight, carWidth, roofLength, roofHeig
     var windowMaterial = new THREE.MeshBasicMaterial({ color: 0xcce6ff, wireframe: false });
 
     //front window
-    geometry = createTriangularPrism(carBlockHeight*2, carWidth);
-    var frontWindow = new THREE.Mesh(geometry, windowMaterial);
+    carGeometry = createTriangularPrism(carBlockHeight*2, carWidth);
+    var frontWindow = new THREE.Mesh(carGeometry, windowMaterial);
     frontWindow.position.set(x + windowPosition , y + roofHeight , z);
     obj.add(frontWindow);
 
     //back window
-    geometry = createTriangularPrism(carBlockHeight*2,carWidth);
-    var backWindow = new THREE.Mesh(geometry, windowMaterial);
+    carGeometry = createTriangularPrism(carBlockHeight*2,carWidth);
+    var backWindow = new THREE.Mesh(carGeometry, windowMaterial);
     backWindow.rotateY(Math.PI);
     backWindow.position.set(x - windowPosition, y + roofHeight , z);
     obj.add(backWindow);
 }
 
 
-function addCarWheels(obj, x, y, z, carBlockHeight, wheelPosition, wheelRadius, wheelWidth, carWidth){
+function addCarWheels (obj, x, y, z, carBlockHeight, wheelPosition, wheelRadius, wheelWidth, carWidth){
 
     var wheelMaterial = new THREE.MeshBasicMaterial({ color: 0xb3b3cc, wireframe: false });
-    geometry = new THREE.TorusGeometry( wheelRadius , wheelWidth, 16,100);
+    var carGeometry = new THREE.TorusGeometry( wheelRadius , wheelWidth, 16,100);
+    var wheel;
 
-    wheel = new THREE.Mesh(geometry, wheelMaterial);
+    wheel = new THREE.Mesh(carGeometry, wheelMaterial);
     wheel.position.set(x + wheelPosition +carBlockHeight, y - carBlockHeight/2, z + carWidth/2 + wheelWidth);
     obj.add(wheel);
 
-    wheel = new THREE.Mesh(geometry, wheelMaterial);
+    wheel = new THREE.Mesh(carGeometry, wheelMaterial);
     wheel.position.set(x - wheelPosition - carBlockHeight, y - carBlockHeight/2, z + carWidth/2 + wheelWidth);
     obj.add(wheel);
 
-    wheel = new THREE.Mesh(geometry, wheelMaterial);
+    wheel = new THREE.Mesh(carGeometry, wheelMaterial);
     wheel.position.set(x - wheelPosition - carBlockHeight, y - carBlockHeight/2, z - carWidth/2 - wheelWidth);
     obj.add(wheel);
 
-    wheel = new THREE.Mesh(geometry, wheelMaterial);
+    wheel = new THREE.Mesh(carGeometry, wheelMaterial);
     wheel.position.set(x + wheelPosition + carBlockHeight, y - carBlockHeight/2, z - carWidth/2 - wheelWidth);
     obj.add(wheel);
 }
@@ -130,6 +154,3 @@ function addCarWheels(obj, x, y, z, carBlockHeight, wheelPosition, wheelRadius, 
 
 
 
-function createCar(x, y, z) {
-    scene.add( createBasicCar(x,y,z) );
-}
